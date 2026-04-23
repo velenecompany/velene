24cb0dd5454451714e80e14ed0cf4d3f5ef06209
velenecompany <velacontacto2@gmail.com>
2026-04-23 01:09:51 -0600

    import withTransaction in webhook

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
-import db from "@/lib/db";
+import db, { withTransaction } from "@/lib/db";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2023-10-16' });
 
