e083da131e03912a1693b9674e5a64416bf34c00
velenecompany <velacontacto2@gmail.com>
2026-04-26 01:06:13 -0600

    remove checkout auth requirement

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const PROTECTED = ['/account', '/checkout'];
+const PROTECTED = ['/account'];
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
