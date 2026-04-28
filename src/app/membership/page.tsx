afcc9ecfe29b742f2752a596356f8057b14532ff
velenecompany <velacontacto2@gmail.com>
2026-04-27 19:25:11 -0600

    Fix import useEffect en membership

--- src/app/membership/page.tsx
+++ src/app/membership/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
 import Link from 'next/link';
